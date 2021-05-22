6b3845a36eccd3fb2786422d92e65eb701e4c918
Joosep <joosep.parts@gmail.com>
2021-05-22 21:15:41 +0300

    router

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,21 +1,10 @@
-import {EventEmitter, Output} from '@angular/core';
-import {Component, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
- @Output()  featureSelected = new EventEmitter<string>();
+export class HeaderComponent {
 
-  constructor() {
-  }
-
-  ngOnInit(): void {
-  }
-
-  onSelect(feature: string) {
-    this.featureSelected.emit(feature)
-  }
 }
